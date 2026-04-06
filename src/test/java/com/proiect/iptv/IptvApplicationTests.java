6cc4e56bef424bc740667961793da69f6df8b559
POGorilla <rocky0x007@gmail.com>
2026-04-06 10:55:42 +0300

    base added

---
+++ src/test/java/com/proiect/iptv/IptvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiect.iptv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IptvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
